83096c2a1be9e55fadbfcbbfca5427bb3b82ca3e
Ryan Schleck <ryan.m.schleck@gmail.com>
2023-04-27 14:57:29 -0400

    adding Senator class

---
+++ Senator.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+public class Senator {
+    private String name;
+    private double money;
+    private int influence;
+    private ArrayList<Company> companies;
+    private ArrayList<Lobbyist> lobbyists;
+
+    public Senator(String name, double money, int influence) {
+        this.name = name;
+        this.money = money;
+        this.influence = influence;
+        this.companies = new ArrayList<>();
+        this.lobbyists = new ArrayList<>();
+    }
+
+    public boolean castVote(String input) {
+        // Implementation of casting a vote in favor of companies for money
+        return (input.equalsIgnoreCase("y"));
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return this.money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public double subtractMoney(double money) {
+        this.money -= money;
+        return this.money;
+    }
+
+    public double addMoney(double money) {
+        this.money += money;
+        return this.money;
+    }
+
+    public char makeDecision(String decision) {
+        // Implementation of making a multiple-choice decision
+        return decision.charAt(0);
+    }
+
+    public boolean charmColleague(Senator colleague, Lobbyist lobbyist) {
+        // Implementation of attempting to gain influence by introducing a lobbyist to a colleague
+        return true;
+    }
+
+    public void addCompany(Company company) {
+        this.companies.add(company);
+    }
+
+    public ArrayList<Company> getCompanies() {
+        return this.companies;
+    }
+
+    public void addLobbyist(Lobbyist lobbyist) {
+        this.lobbyists.add(lobbyist);
+    }
+
+    public ArrayList<Lobbyist> getLobbyists() {
+        return this.lobbyists;
+    }
+}
