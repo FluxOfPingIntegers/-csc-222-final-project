c6328d5679c71e55e264717f52033f947f3a1d5a
Ryan Schleck <ryan.m.schleck@gmail.com>
2023-04-27 15:30:01 -0400

    updating Main

--- Main.java
+++ Main.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    String input = "";
+// TODO: properly import these classes.
+import Company;
+import Lobbyist;
+import Senator;
 
-    // Create a new company with two employees (lobbyists)
-    Lobbyist[] employees = {new Lobbyist("John Smith", null), new Lobbyist("Jane Doe", null)};
-    Company company = new Company("Acme Corporation", employees, null);
-    
-    // Create a new lobbyist
-    Lobbyist lobbyist = new Lobbyist("Bob Johnson", company);
-    
-    // Create a new senator
-    Senator senator = new Senator("John Smith", 10000.0, 5, new Company[]{company}, new Lobbyist[]{lobbyist});
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
 
-    while (!input.equals("q")) {
-        System.out.print("Enter a command (q to quit): ");
-        input = scanner.nextLine();
+        // Create a new company with two employees (lobbyists)
+        Lobbyist[] employees = {new Lobbyist("John Smith", null), new Lobbyist("Jane Doe", null)};
+        Company company = new Company("Acme Corporation", employees, null);
         
-        // Do something based on the user's input
-        // ...
+        // Create a new lobbyist
+        Lobbyist lobbyist = new Lobbyist("Bob Johnson", company);
+        
+        // Create a new senator
+        Senator senator = new Senator("John Smith", 10000.0, 5, new Company[]{company}, new Lobbyist[]{lobbyist});
+
+        while (!input.equals("q")) {
+            System.out.print("Enter a command (q to quit): ");
+            input = scanner.nextLine();
+            
+            // Do something based on the user's input
+            // ...
+        }
+        
+        System.out.println("Farewell!");
     }
-    
-    System.out.println("Farewell!");
 }
+
